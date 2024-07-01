6178c0f88be9554ac5cc6e6d976de19b19940776
PRIYANKAec <priyankaa261103@gmail.com>
2024-07-01 13:14:02 +0530

    Implemented Graph Visualize for Deals

--- breezy-feet-grin/src/components/index.ts
+++ breezy-feet-grin/src/components/index.ts
@@ -1,10 +1,13 @@
 import UpcomingEvents from "./home/upcoming-events";
 import DealsChart from "./home/deals-chart";
+
 import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
 import AccordionHeaderSkeleton from "./skeleton/accordion-header";
 import KanbanColumnSkeleton from "./skeleton/kanban";
 import LatestActivitiesSkeleton from "./skeleton/latest-activities";
 import ProjectCardSkeleton from "./skeleton/project-card";
+
+import DashboardTotalCountCard from "./home/total-count-card";
 
 export { 
     UpcomingEvents, 
@@ -15,4 +18,6 @@ export {
     KanbanColumnSkeleton,
     ProjectCardSkeleton,
     LatestActivitiesSkeleton,
+
+    DashboardTotalCountCard,
 };
