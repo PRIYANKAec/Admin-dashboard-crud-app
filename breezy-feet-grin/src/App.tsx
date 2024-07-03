6b983b93abe520dbef0496d09ac8b67d08479518
PRIYANKAec <priyankaa261103@gmail.com>
2024-07-03 21:29:13 +0530

    Implemented the task edit, delete,contactsTable using Kanban

--- breezy-feet-grin/src/App.tsx
+++ breezy-feet-grin/src/App.tsx
@@ -6,24 +6,26 @@ import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
 import { authProvider, dataProvider, liveProvider } from "./providers";
-import { Home , ForgotPassword, Login, Register, CompanyList } from "./pages";
+import { Home, ForgotPassword, Login, Register, CompanyList } from "./pages";
 
 import routerBindings, {
   CatchAllNavigate,
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
-import { App as AntdApp} from "antd";
+import { App as AntdApp } from "antd";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
 import { resources } from "./config/resources";
 import Create from "./pages/company/create";
-import List from "./tasks/list";
+import Edit from "./pages/company/edit";
+import List from "./pages/tasks/list";
+import EditTask from "./pages/tasks/edit";
+import CreateTask from "./pages/tasks/create";
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
           <AntdApp>
             <DevtoolsProvider>
@@ -38,7 +40,7 @@ function App() {
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
                   useNewQueryKeys: true,
-                  projectId: "X1XYBA-60WnV5-oouDSy",
+                  projectId: "WVyLEd-4karEq-tItoeC",
                   liveMode: "auto",
                 }}
               >
@@ -47,28 +49,29 @@ function App() {
                   <Route path="/login" element={<Login />} />
                   <Route path="/forgot-password" element={<ForgotPassword />} />
                   <Route
-                  element= {
-                  <Authenticated
-                  key="authenticated-layout"
-                  fallback={<CatchAllNavigate  to="/login"/>}
-                  >
-                    <Layout>
-                      <Outlet />
-                    </Layout>
-                  </Authenticated>
-                  }>
-                    <Route index element={<Home />} />
-                    <Route path="/companies"  >
-                    <Route index element={<CompanyList />} />
-                    <Route path="new" element={<Create />}/>
-                    <Route path="edit/:id" element={<Create />}/>
-                    </Route>
-                    <Route path="/tasks" element={
+                    element={
+                    <Authenticated 
+                      key="authenticated-layout"
+                      fallback={<CatchAllNavigate to="/login" />}
+                    >
+                      <Layout>
+                        <Outlet />
+                      </Layout>
+                    </Authenticated>
+                    }>
+                      <Route index element={<Home />} />
+                      <Route path="/companies" >
+                        <Route index element={<CompanyList />} />
+                        <Route path="new" element={<Create />} />
+                        <Route path="edit/:id" element={<Edit />} />
+                      </Route>
+                      <Route path="/tasks" element={
                         <List>
                           <Outlet />
                         </List>
                       }>
-                        
+                        <Route path="new" element={<CreateTask />} />
+                        <Route path="edit/:id" element={<EditTask />} />
                       </Route>
                   </Route>
                 </Routes>
